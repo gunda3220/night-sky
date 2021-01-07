d09980e28771a737de7c471e8271ea363854a2e6
gunda <gundakamalnath@gmail.com>
2021-01-07 16:30:26 +0530

    Falling stars with trails added

--- app.js
+++ app.js
@@ -1,110 +1,130 @@
-// var c = document.getElementById("canvas");
-// var ctx = c.getContext("2d");
-// var cwidth, cheight;
-// var shells = [];
-// window.onresize = function(){
-//   reset();
-// }
-// reset();
-// function reset(){
-//   cwidth = window.innerWidth;
-//   cheight = window.innerHeight;
-//   c.width = cwidth;
-//   c.height = cheight;
-// }
-// function newShell(){
-//   var left = (Math.random() > 0.5);
-//   var shell = {};
-//   shell.x = (1*left);
-//   shell.y = 0.3;
-//   shell.xoff = (0.01+Math.random()*0.007)*(left ? 1:-1);
-//   shell.yoff = 0.01+ Math.random()*0.007;
-//   shell.size = Math.random()*6 + 3;
-//   shell.color = '#ffffff';
-//   shells.push(shell);
-// }
-// var lastRun = 0;
-// Run();
-// function Run(){
-//   var dt = 1;
-//   if(lastRun != 0)
-//   {
-//     dt = Math.min(50,(performance.now()-lastRun));
-//   }
-//   lastRun = performance.now();
-//   ctx.fillStyle = "rgba(0,0,0,0.25)";
-//   ctx.fillRect(0,0,cwidth,cheight);
-//   if((shells.length < 10) && (Math.random() > 0.9))
-//   {
-//     console.log("inside new shell");
-//     newShell();
-//   }
-//   for (let ix in shells)
-//   {
-//     var shell = shells[ix];
-//     ctx.beginPath();
-//     ctx.arc(shell.x*cwidth,shell.y*cheight,shell.size,0,2*Math.PI);
-//     ctx.fillStyle = shell.color;
-//     ctx.fill();
-//     shell.x -=shell.xoff;
-//     shell.y -= shell.yoff;
-//     shell.xoff -= (shell.xoff*dt*0.001);
-//     shell.yoff -= ((shell.yoff+0.2)*dt*0.00005);
-//     if(shell.yoff < -0.005){
-//       // newPass(shell);
-//       shells.splice(ix,1);
-//     }
-//   }
-//   // for(let ix in pass){
-//   //   var pas = pass[ix];
-//   //   ctx.beginPath();
-//   //   ctx.arc(pas.x,pas.y,pas.size,0,2*Math.PI);
-//   //   ctx.fillStyle = pas.color;
-//   //   ctx.fill();
-//   //   pas.x -= pas.xoff;
-//   //   pas.y -= pas.yoff;
-//   //   pas.xoff -= (pas.xoff*dt*0.001);
-//   //   pas.yoff -= ((pas.yoff + 5)*dt*0.005);
-//   //   pas.size -= (dt*0.002*Math.random())
-//   //   if((pas.y > cheight) || (pas.y < -50) || (pas.size <= 0)){
-//   //     pass.splice(ix,1);
-//   //   }
-//   // }
-//   requestAnimationFrame(Run);
-// }
 var stars = [];
-var c = document.getElementById("canvas");
-var ctx = c.getContext("2d");
+var canvas = document.getElementById("canvas");
+canvas1 = document.getElementById("canvas1");
+var ctx = canvas.getContext("2d");
+var ctx1 = canvas1.getContext("2d");
 var cwidth, cheight;
+var randomTime = 0;
+var yArray = [];
+var sum = 0;
+var temp = 0;
+var diff = 0;
 window.onresize = function(){
   reset();
+  insertStars();
 }
 reset();
 function reset(){
   cwidth = window.innerWidth;
   cheight = window.innerHeight;
-  c.width = cwidth;
-  c.height = cheight;
+  canvas.width = cwidth;
+  canvas.height = cheight;
+  canvas1.width = cwidth;
+  canvas1.height = cheight;
 }
 function newStar(){
   var star = {};
   star.x = Math.random()*cwidth;
   star.y = (Math.random()*cheight*0.5);
-  star.size = (Math.random()*6+3);
-  star.color = '#ffffff';
+  star.radius = (Math.random()*1.5+0.3);
   stars.push(star);
 }
-for(let i = 0;i< 120;i++)
-{
-  newStar();
+function insertStars(){
+  stars = [];
+  for(let i = 0;i< 250;i++)
+  {
+    newStar();
+  }
+  for(let ix in stars){
+    var star = stars[ix];
+    ctx.beginPath();
+    ctx.arc(star.x,star.y,star.radius,0,Math.PI*2);
+    ctx.fillStyle = "#ffffff";
+    ctx.fill();
+  }
 }
-for(let ix in stars){
-  var star = stars[ix];
-  ctx.moveTo(star.x,star.y);
-  ctx.lineTo(star.x+1, star.y+1);
-  console.log(star.x+","+star.y);
-  ctx.fillStyle = star.color;
-  ctx.strokeStyle = "white";
-  // ctx.stroke();
-  ctx.fill();
+insertStars();
+randomTime = 1000;
+var starTiming = setInterval(fallingStar,randomTime);
+var fStar = {};
+var requestId;
+var left = 0;
+var yPos = 0;
+var dt = 0;
+function fallingStar(){
+  left = (Math.random() > 0.5);
+  fStar.x = Math.random()*cwidth;
+  dt = (fStar.x>cwidth/2) ? -1 : 1;
+  yPos = Math.random() > 0.5;
+  fStar.y = cheight/2*(yPos);
+  temp = fStar.y;
+  fStar.radius = Math.random()*1.8+0.3;
+  ctx1.beginPath();
+  ctx1.arc(fStar.x,fStar.y,fStar.radius,0,Math.PI*2);
+  ctx1.fillStyle = "#ffffff";
+  ctx1.fill();
+  animate();
+  sum = 0;
+  diff = 0;
+  clearInterval(starTiming);
+  randomTime = Math.random()*4000+4000;
+  starTiming = setInterval(fallingStar,randomTime);
+}
+function animate(){
+  requestId = window.requestAnimationFrame(animate);
+  fStar.x += 3*dt;
+  if(yPos)
+  {
+    fStar.y -= Math.pow((fStar.x*3/cwidth),2);
+  }
+  else{
+    fStar.y += Math.pow((fStar.x*3/cwidth),2);
+  }
+
+  diff = Math.abs(fStar.y - temp);
+  diff = diff/fStar.y;
+  yArray.push(diff);
+
+  temp = fStar.y;
+
+  if(yArray.length > 5){
+    yArray.pop();
+  }
+  if(yArray.length >= 5)
+  {
+    for(let i = 0;i<yArray.length;i++)
+    {
+      sum += yArray[i];
+    }
+  }
+    if(dt > 0)
+    {
+      if(!yPos)
+      {
+        ctx1.clearRect(0,0,fStar.x-20,fStar.y-20);
+      }
+      else{
+        ctx1.clearRect(0,0,fStar.x-20,cheight);
+      }
+    }
+    else{
+      if(yPos)
+      {
+        ctx1.clearRect(fStar.x+30,0,cwidth - fStar.x - 30,cheight);
+      }
+      else{
+        ctx1.clearRect(fStar.x+30,0,cwidth-fStar.x-30,cheight);
+      }
+    }
+
+  ctx1.beginPath();
+  ctx1.arc(fStar.x,fStar.y,fStar.radius,0,Math.PI*2);
+  ctx1.fillStyle = "#ffffff";
+  ctx1.fill();
+  if((fStar.y > cheight/2) || (fStar.y < 0) || (fStar.x > cwidth) || (fStar.x < 0)){
+    window.cancelAnimationFrame(requestId);
+    ctx1.clearRect(0,0,cwidth,cheight)
+    ctx1.fillStyle = "#000000";
+    ctx1.fill();
+  }
 }
